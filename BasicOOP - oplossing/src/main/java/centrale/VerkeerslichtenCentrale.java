558add4515499bb3eff51c9c38e4a0c959f42f35
maartentroost <maarten.troost@odisee.be>
2023-05-15 12:24:34 +0200

    oplossing

---
+++ BasicOOP - oplossing/src/main/java/centrale/VerkeerslichtenCentrale.java
@@ -0,0 +1,85 @@
+package centrale;
+import java.util.ArrayList;
+import java.util.List;
+
+/** Stelt een verkeerlichtencentrale voor van de simulatie */
+public class VerkeerslichtenCentrale {
+
+    //Corrigeer de code denkende aan static, final, access modifiers, information hiding en SRP
+    // Er is ook 1 lijn overbodige code (met verkeerde resultaten tot gevolg)
+    // en 1 logische fout
+
+    //** Bepaald of de centrale actief is.*/
+    private boolean isActive = false;
+    final private int aantalLichten = 3;
+    /**
+     * Welke verkeerslichten zijn aan? De lichten zijn strikt oplopend genummerd.
+     */
+    private final List<Boolean> verkeerslichtenActief = new ArrayList<Boolean>(aantalLichten);
+
+    // De code van deze class voldoet aan de centralestandaard versie 1.2a
+    static final private String versie = "1.2a";
+
+    /** aan welke centralestandaard deze class voldoet
+     * @return versienummer
+     */
+    static public String getCentraleVersie() {
+        return versie;
+    }
+
+    public VerkeerslichtenCentrale() {
+        initialize();
+        isActive = true;
+    }
+
+    /**
+     * Maakt de verkeerslichten aan
+     */
+    private void initialize() {
+        for (int i = aantalLichten; i > 0; i--) {
+            verkeerslichtenActief.add(true);
+        }
+    }
+
+    /**
+     * Schakel een verkeerslicht uit
+     * @param lichtnr volgnr van het uit te schakelen licht
+     */
+    public void schakelLichtUit(final int lichtnr) {
+        int lichtnrUitTeSchakelen = lichtnr - 1;
+        if (lichtnrUitTeSchakelen >= verkeerslichtenActief.size()) lichtnrUitTeSchakelen = verkeerslichtenActief.size()-1; //logische fout
+        if (isActive) verkeerslichtenActief.set(lichtnrUitTeSchakelen, Boolean.FALSE);
+    }
+
+    /**
+     * Is dit licht actief?
+     * @param lichtNr te controleren licht
+     * @return actief of niet
+     */
+    public boolean isVerkeerslichtActief(final int lichtNr) {
+        return verkeerslichtenActief.get(lichtNr>=aantalLichten?aantalLichten-1:lichtNr-1);
+    }
+
+    /** Het aantal lichten beheert door de centrale
+     * @return aantal
+     */
+    public int getAantalLichten() {
+        return aantalLichten;
+    }
+}
+
+/** de simulatie. Afgesplitst van verkeerlichtenCentrale want elek class mag maar 1 verantwoordelijkheid hebben */
+class Simulatie {
+    /** start de simulatie */
+    public static void main(String[] args) {
+        final VerkeerslichtenCentrale centrale1=new VerkeerslichtenCentrale();
+        //centrale1.initialize();  // iw is verantwoordelijk om te weten hoe een centrale intern werkt? → de VerkeerslichtenCentrale
+
+        centrale1.schakelLichtUit(3);
+
+        System.out.println("centralestandaard versie: "+ VerkeerslichtenCentrale.getCentraleVersie());
+        for (int i=1;i<=centrale1.getAantalLichten();i++) {
+            System.out.println("verkeerslicht "+( centrale1.isVerkeerslichtActief(i)?"aan":"uit"));
+        }
+    }
+}
